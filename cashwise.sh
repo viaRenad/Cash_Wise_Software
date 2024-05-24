715397d5adbae2093de9cde3f18bf09ccf86418f
reenadd <renadalansari2@gmail.com>
2024-05-24 23:22:29 +0000

    Layan

--- cashwise.sh
+++ cashwise.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Function That Acts as The Welcome Screen
-function welcome_screen()
+
+function welcome_screen() 
 {
-    # Display The Welcome Message + Software's Slogan
     clear
     echo "======================================================================="
     echo "                        Welcome to Cash Wise                           "
@@ -10,83 +9,103 @@ function welcome_screen()
     echo "      The ultimate tool for smarter saving and seamless spending       "
     echo "======================================================================="
     echo ""
-    # Options for Loging In or Creating an Account
-    echo -n "Enter Your Choice: "
-    read choice
+    while (true)
+    do
+        echo "1. Login"
+        echo "2. Create an Account"
+        echo "3. Exit"
+        echo ""
+        read -p "Enter Your Choice: " choice
+        echo ""
+        
     if [ "$choice" == "1" ]; then
         login
     elif [ "$choice" == "2" ]; then
         create_account
+    elif [ "$choice" == "3" ]; then
+        exit
     else
+        echo "Invalid Option, Please Try Again"
         echo ""
-        echo "Invalid Choice, Please Try Again"
-        echo ""
-        welcome_screen
     fi
+    done
 }
 
-# Funtion to Create an Account
-function create_account()
+function create_account() 
 {
     read -p "Enter First Name: " first_name
     read -p "Enter Last Name: " last_name
 
-    while true; do
-        read -p "Enter Email: " email
-        if [[ "$email" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]; then
+    while (true)
+    do
+        read -p "Enter Your G-mail: " gmail
+        
+        gmail_pattern="^[a-zA-Z0-9._%+-]+@gmail\.com$"
+        
+        if [[ "$gmail" =~ $gmail_pattern ]]; then
             break
         else
-            echo "Invalid Email Adress, Please Try Again"
+            echo ""
+            echo "Invalid G-mail Address, Please Try Again"
+            echo ""
         fi
     done
 
-    while true; do
-        read -p "Set a Password of at Least 8 Ccharacters: " password
+    while (true)
+    do
+        read -p "Set a Password of at Least 8 Characters: " password
+        
         if [ ${#password} -ge 8 ]; then
             break
         else
+            echo ""
             echo "Password Too Short, Please Try Again"
+            echo ""
         fi
     done
 
-    while true; do
+    while (true)
+    do
         read -p "Create a Username: " username
-        if grep -q "^$username:" users.txt; then
+        
+        if grep "^$username:" users.txt >/dev/null; then
+            echo ""
             echo "Username Already Taken, Please Choose a Different Username"
+            echo ""
         else
             break
         fi
     done
 
-    echo "$username:$password:$first_name:$last_name:$email" >>users.txt
+    echo "$username:$password:$first_name:$last_name:$gmail" >> users.txt
+    echo ""
     echo "Account Created Successfully!"
     echo ""
     welcome_screen
 }
-# Function To Handle User Log In Process
-function login()
+
+function login() 
 {
-    # Check username and password from the users.txt file
     echo -n "Enter your username: "
     read username
     echo -n "Enter your password: "
     read password
-    if grep -q "^$username:$password:" users.txt; then
+    echo ""
+    
+    if grep "^$username:$password:" users.txt >/dev/null; then
         echo "Login successful!"
-        show_menu
+        show_menu "$username"
     else
         echo "Invalid username or password."
         echo ""
-        echo "1. Try again"
-        echo "2. Go back to the welcome screen"
-        read -p "Enter your choice: " retry_option
-        if [ "$retry_option" == "1" ]; then
+        echo "Try again? Press (1) for Yes and Press any other Key for No"
+        echo ""
+        read -p "Enter your choice: " choice
+        echo ""
+        if [ "$choice" == "1" ]; then
             login
-        elif [ "$retry_option" == "2" ]; then
-            welcome_screen
         else
-            echo "Invalid option. Please try again."
-            login
+            welcome_screen
         fi
     fi
 }
